4e6046a82175ee21bba5fd021adc81430f5c56fe
Bijan Haney <bijan@viam.com>
2023-11-20 12:18:26 -0500

    update away from golog

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ package main
 import (
 	"context"
 
-	"github.com/edaniels/golog"
 	"github.com/viam-labs/overlay-fps/overlayfps"
 	"go.viam.com/rdk/components/camera"
+	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
 	"go.viam.com/utils"
 )
@@ -15,7 +15,7 @@ func main() {
 	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("overlay-fps"))
 }
 
-func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) (err error) {
+func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
 	oMod, err := module.NewModuleFromArgs(ctx, logger)
 	if err != nil {
 		return err
